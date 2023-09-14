1440f4d8b3a69fdce495d5025d74d9bd2b5c75bf
MagliariElio <magliari.elio08@gmail.com>
2023-09-14 17:14:53 +0200

    feat: modify path of the image

--- application/src/custom_widget/mod.rs
+++ application/src/custom_widget/mod.rs
@@ -8,6 +8,8 @@ mod resizable_box;
 mod custom_slider;
 mod delay_button;
 
+mod alert;
+
 pub use colored_button::ColoredButton;
 pub use selected_rect::SelectedRect;
 pub use custom_zstack::{CustomZStack,OverImages,CREATE_ZSTACK,SAVE_OVER_IMG,SHOW_OVER_IMG,UPDATE_COLOR,UPDATE_BACK_IMG};
@@ -16,4 +18,5 @@ pub use take_screenshot_button::{TakeScreenshotButton,SAVE_SCREENSHOT};
 pub use resizable_box::{ResizableBox,UPDATE_ORIGIN};
 pub use custom_slider::CustomSlider;
 pub use delay_button::{DelayButton,DELAY_SCREENSHOT};
+pub use alert::{Alert};
 
